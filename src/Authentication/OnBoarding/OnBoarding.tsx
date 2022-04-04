08ab8d0fd9dc69f82ecce9aaa888bc8274289121
Rayper <raihangunawan22@gmail.com>
2022-04-04 12:10:50 +0700

    Setup Assets, etc

---
+++ src/Authentication/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const OnBoarding = () => {
+    return (
+        <View style={{ flex: 1, backgroundColor: "cyan" }}></View>
+    )
+}
+
+export default OnBoarding;
